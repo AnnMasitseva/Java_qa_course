6c1bc0573914b06806e9036a8d636b5fd6268efa
AnnMasitseva <124693729+AnnMasitseva@users.noreply.github.com>
2023-02-14 20:35:58 +0300

    exercise 2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,8 +3,17 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
     
     public static void main(String[] args) {
+        Point p1 = new Point(2, 1);
+        Point p2 = new Point(5, 6);
+        System.out.println("Расстояние между двумя точками с координатами " + '(' + p1.x + ";" + p1.y + ')'  + " и " + '(' + p2.x + ";" + p2.y + ')' + " = " + distance(p1,p2));
 
-        System.out.println("Hello, world!");
+        PointMetod p = new PointMetod(2, 1, 5,6);
+        System.out.println("Расстояние между двумя точками p1 и p2 " + p.distance());
+
+    }
+
+   public static double distance(Point p1, Point p2){
+        return Math.sqrt((Math.pow((p2.x-p1.x),2)+Math.pow((p2.y-p1.y),2)));
     }
 }
     
