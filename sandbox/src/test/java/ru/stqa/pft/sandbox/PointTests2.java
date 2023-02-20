519dc631082698152e279b354e91e0eb6c735986
AnnMasitseva <124693729+AnnMasitseva@users.noreply.github.com>
2023-02-20 17:15:27 +0300

    exercise 3 update

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests2.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests2.java
@@ -3,12 +3,11 @@ package ru.stqa.pft.sandbox;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class PoinTests {
-
+public class PointTests2 {
     @Test
     public void testDistance(){
         Point p1 = new Point(2, 1);
         Point p2 = new Point(5, 6);
-        assert p1.distance1(p2) == 5.830951894845301;
+        Assert.assertEquals(p1.distance1(p2),5.830951894845301);
     }
 }
