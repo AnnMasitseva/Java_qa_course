15e51ca270d0d581ab1b80cbf1b0fc729507ca9d
AnnMasitseva <124693729+AnnMasitseva@users.noreply.github.com>
2023-03-22 16:31:34 +0300

    exsercise 13 Data Provider for: - ContactCreationTests - GroupCreationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,36 +1,73 @@
 package ru.stqa.pft.addressbook.tests;
 
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
+import java.io.*;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
-  @Test
-  public void testContactCreationTests() throws Exception {
-    app.goTo().groupPage();
-    if (app.group().all().size() == 0 || ! app.group().isThereAGroupName("test2"))  {
-        app.group().create(new GroupData().withName("test2"));
+
+    @DataProvider
+    public Iterator<Object[]> validContactsFromXml() throws IOException {
+        String xml;
+        try (BufferedReader reader = new BufferedReader(new FileReader((new File("src/test/resources/contacts.xml"))))) {
+            xml = "";
+            String line = reader.readLine();
+        }
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        xstream.allowTypes(new Class[]{ContactData.class});
+        List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+        return contacts.stream().map((c) -> new Object[]{c}).collect(Collectors.toList()).iterator();
     }
-        app.goTo().homePage();
+
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+      try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")))) {
+          String json = "";
+          String line = reader.readLine();
+          while (line != null) {
+              json += line;
+              line = reader.readLine();
+          }
+          Gson gson = new Gson();
+          List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {}.getType());
+          return contacts.stream().map((с) -> new Object[]{с}).collect(Collectors.toList()).iterator();
+      }
+  }
+
+  @Test(dataProvider = "validContactsFromJson")
+  public void testContactCreationTests(ContactData contact) {
+    app.goTo().groupPage();
+   if (app.group().all().size() == 0 || ! app.group().isThereAGroupName("test2")){
+      app.goTo().groupPage();
+      app.group().create(new GroupData().withName("test2"));
+    }
+    app.goTo().homePage();
     Contacts before = app.contact().all();
-    ContactData contact = new ContactData()
-            .withFirstname("Anna").withMiddlename("Aleksandrovna").withLastname("Masitseva").withAddress("St.Peterburg")
-            .withMobilePhone("+79009009090").withEmail("email@domain.com").withGroup("test2");
+    //File photo = new File("src/test/resources/cat.png");
     app.contact().create(contact);
     app.goTo().homePage();
-    assertThat(app.contact().counter(), equalTo(before.size()+1));
     Contacts after = app.contact().all();
-
+    System.out.println(after);
+    assertThat(app.contact().count(), equalTo(before.size()+1));
     assertThat(after, equalTo(
             before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
   }
 
-  @Test
+  @Test(enabled = false)
   public void testContactBadCreationTests() throws Exception {
     app.goTo().groupPage();
     if (app.group().all().size() == 0 || ! app.group().isThereAGroupName("test2"))  {
@@ -42,10 +79,11 @@ public class ContactCreationTests extends TestBase {
             .withFirstname("Anna'").withGroup("test2");
     app.contact().create(contact);
     app.goTo().homePage();
-    assertThat(app.contact().counter(), equalTo(before.size()));
+    assertThat(app.contact().count(), equalTo(before.size()));
     Contacts after = app.contact().all();
     assertThat(after, equalTo(before));
   }
+
 }
 
 
